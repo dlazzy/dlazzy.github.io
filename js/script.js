00c753bb3f39fe8e37ede544f7d0a53b2567db10
Valentin <valentblanchard@gmail.com>
2017-04-27 15:37:52 +0200

    That's it

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$('#dlazzy').typeIt({
+     speed: 60,
+     autoStart: false,
+     lifeLike: true,
+     loop: true,
+})
+.tiPause(200)
+.tiType('My name is Dlazzy')
+.tiPause(1000)
+.tiDelete()
+.tiType('That\'s it')
+.tiPause(750)
+.tiDelete()
